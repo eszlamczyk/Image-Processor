5abcf5f2728925094f8a48f14b3bf4e1145e99b8
Ernest Szlamczyk <ernest.szlamczyk@gmail.com>
2025-02-20 14:11:37 +0100

    Added automatic directory creation for images

--- imageprocesor/src/main/java/pl/ernest/imageprocesor/config/PathConfig.java
+++ imageprocesor/src/main/java/pl/ernest/imageprocesor/config/PathConfig.java
@@ -4,13 +4,24 @@ package pl.ernest.imageprocesor.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Configuration
 public class PathConfig {
-    @Value("${image.full.path}")
-    private String fullImagePath;
 
-    @Value("${image.miniature.path}")
-    private String miniatureImagePath;
+    private final String fullImagePath;
+
+    private final String miniatureImagePath;
+
+    public PathConfig(@Value("${image.miniature.path}") String miniatureImagePath,
+                      @Value("${image.full.path}") String fullImagePath)
+            throws IOException {
+        this.miniatureImagePath = miniatureImagePath;
+        this.fullImagePath = fullImagePath;
+        createFoldersIfNeeded();
+    }
 
     public String getFullImagePath(){
         return fullImagePath;
@@ -18,6 +29,12 @@ public class PathConfig {
 
     public String getMiniatureImagePath(){
         return miniatureImagePath;
+    }
+
+
+    private void createFoldersIfNeeded() throws IOException {
+        Files.createDirectories(Paths.get(fullImagePath));
+        Files.createDirectories(Paths.get(miniatureImagePath));
     }
 
 }
